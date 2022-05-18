e020b0b5e708b885f3eb43d90ddbf0fc06db239e
rayo17 <61331133+rayo17@users.noreply.github.com>
2022-05-18 07:53:52 -0600

    Creacion de los eventos de la clase Screen

--- screen.cpp
+++ screen.cpp
@@ -15,9 +15,9 @@ Screen::Screen(QWidget *parent) :
 {
    ui->setupUi(this);
    enable=false;
-   //color=QColor(Qt::black);
-  // size=5;
-  //Create matrix where each element will be a pixel value.
+   color=QColor(Qt::black);
+   size=5;
+
  //qDebug()<<"matrix"<<**matrix;
 }
 
@@ -35,3 +35,62 @@ void Screen::createScreen(int x, int y){
     painter=new QPainter(img);
     painter->fillRect(img->rect(),Qt::white);
 }
+
+
+//eventos de la pantalla para dibujar
+void Screen::paintEvent(QPaintEvent *event){
+
+    enable=true;
+    QPainter painter2(this);
+
+
+    painter2.drawImage(0,0,*img);
+    event->accept();
+}
+void Screen::mousePressEvent(QMouseEvent *event){
+    qDebug()<<event->pos();
+    int x=event->pos().x();
+   int y=event->pos().y();
+    matrix[x][y]=qGray(img->pixel(x,y));
+
+   qDebug()<<"("<<x<<","<<y<<")"<<"color"<<matrix[x][y];
+    enable=true;
+    begin=event->pos();
+    event->accept();
+
+}
+void Screen::mouseMoveEvent(QMouseEvent *event){
+
+    qDebug()<<event->pos();
+
+
+    if(!enable){
+
+        event->accept();
+        return;
+    }
+    else{
+        pencil.setColor(color);
+        pencil.setCapStyle(Qt::RoundCap);
+        pencil.setWidth(size);
+        end=event->pos();
+        int x=event->pos().x();
+       int y=event->pos().y();
+        matrix[x][y]=qGray(img->pixel(x,y));
+
+       qDebug()<<"("<<x<<","<<y<<")"<<"color"<<matrix[x][y];
+
+        painter->setPen(pencil);
+
+        painter->drawLine(begin,end);
+        begin=end;
+        update();
+        event->accept();
+    }
+}
+void Screen::mouseReleaseEvent(QMouseEvent *event){
+    enable=false;
+
+    event->accept();
+}
+
